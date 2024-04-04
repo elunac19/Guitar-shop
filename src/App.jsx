fe66655980a4e42c56f5e99cbfd0b53fce23bd30
elunac19 <elunac19@gmail.com>
2024-04-04 12:36:04 -0600

    Finishing Cart Methods

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,33 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Header from "./components/Header"
 import Guitar from "./components/Guitar"
 import { db } from "./data/db"
 
 function App() {
 
+  const initialCart = () => {
+    const localStorageCart = localStorage.getItem('cart')
+    return localStorageCart ? JSON.parse(localStorageCart) : []
+  }
   //Only when simulating db
   const [data, setData] = useState(db)
-  const [cart, setCart] = useState([])
+  const [cart, setCart] = useState(initialCart)
+  
+  const MAX_ITEMS = 5
+  const MIN_ITEMS = 1
+
+  useEffect( () => {
+    localStorage.setItem('cart', JSON.stringify(cart))
+  }, [cart])
 
   function addToCart(item){
-    //If the item doesnt exist it returns -1.
-    const itemExists = cart.findIndex((indexItem) => indexItem.id === item.id)
+    //findIndex returns -1 if the element doesnt exists.
+    const itemIndex = cart.findIndex((indexItem) => indexItem.id === item.id)
 
-    if(itemExists >= 0){
+    if(itemIndex >= 0){
       const updateCart = [...cart]    
-      updateCart[itemExists] = {...updateCart[itemExists], quantity: updateCart[itemExists].quantity + 1}
+      if(updateCart[itemIndex].quantity >= MAX_ITEMS) return 
+      updateCart[itemIndex] = {...updateCart[itemIndex], quantity: updateCart[itemIndex].quantity + 1}
       setCart(updateCart)
 
     }else{
@@ -24,7 +36,31 @@ function App() {
   }
 
   function removeFromCart(itemId){
-    setCart([...cart].filter((item)=>item.id !== itemId))
+    setCart(cart.filter((item)=>item.id !== itemId))
+  }
+
+  function increaseItemQuantity(itemId){
+    const updateCart = cart.map((item) => {
+      if(item.id === itemId && item.quantity < MAX_ITEMS){
+        return {...item, quantity: item.quantity + 1}
+      }
+      return item
+    })
+    setCart(updateCart)
+  }
+
+  function decreaseItemQuantity(itemId){
+    const updateCart = cart.map((item) => {
+      if(item.id === itemId && item.quantity > MIN_ITEMS){
+        return {...item, quantity: item.quantity - 1}
+      }
+      return item
+    })
+    setCart(updateCart)
+  }
+
+  function clearCart(){
+    setCart([])
   }
 
   return (
@@ -32,6 +68,9 @@ function App() {
       <Header
         cart={cart}
         removeFromCart={removeFromCart}
+        increaseItemQuantity={increaseItemQuantity}
+        decreaseItemQuantity={decreaseItemQuantity}
+        clearCart={clearCart}
       />
       <main className="container-xl mt-5">
           <h2 className="text-center">Guitar Colletion</h2>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { useMemo } from "react"
 
-function Header({cart, removeFromCart}) {
+function Header({cart, removeFromCart, increaseItemQuantity, decreaseItemQuantity, clearCart}) {
 
     //Derivative State
     const isCartEmpty = useMemo( () => cart.length === 0, [cart] )
@@ -13,14 +13,14 @@ function Header({cart, removeFromCart}) {
                     <div className="row justify-content-center justify-content-md-between">
                         <div className="col-8 col-md-3">
                             <a href="index.html">
-                                <img className="img-fluid" src="./public/img/logo.svg" alt="logo image" />
+                                <img className="img-fluid" src="/img/logo.svg" alt="logo image" />
                             </a>
                         </div>
                         <nav className="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
                             <div 
                                 className="carrito"
                             >
-                                <img className="img-fluid" src="./public/img/carrito.png" alt="cart icon" />
+                                <img className="img-fluid" src="/img/carrito.png" alt="cart icon" />
 
                                 <div id="carrito" className="bg-white p-3">
                                     { isCartEmpty ? (
@@ -51,6 +51,7 @@ function Header({cart, removeFromCart}) {
                                                                 <button
                                                                     type="button"
                                                                     className="btn btn-dark"
+                                                                    onClick={() => decreaseItemQuantity(guitar.id)}
                                                                 >
                                                                     -
                                                                 </button>
@@ -58,6 +59,7 @@ function Header({cart, removeFromCart}) {
                                                                 <button
                                                                     type="button"
                                                                     className="btn btn-dark"
+                                                                    onClick={() => increaseItemQuantity(guitar.id)}
                                                                     >
                                                                     +
                                                                 </button>
@@ -80,6 +82,7 @@ function Header({cart, removeFromCart}) {
                                     )}
                                     <button 
                                         className="btn btn-dark w-100 mt-3 p-2"
+                                        onClick={clearCart}
                                     >
                                         Vaciar Carrito
                                     </button>
